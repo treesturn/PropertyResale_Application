096606ba75ccc0c3f09a0657ed13d8f8e7605888
Triston Chan <77474648+treesturn@users.noreply.github.com>
2022-10-14 16:46:05 +0800

    Added Recycler View for housing list constraint it to btm of guest box

--- app/src/main/java/com/example/propertyresale_app/DatabaseHelper.java
+++ app/src/main/java/com/example/propertyresale_app/DatabaseHelper.java
@@ -113,7 +113,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public Cursor getHousing(){
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
 //        String query = sqLiteDatabase.rawQuery("select password from account_users", null);
-        Cursor c = sqLiteDatabase.rawQuery("select housing_sn, town, resale_price from housing_list limit 10", null);
+        Cursor c = sqLiteDatabase.rawQuery("select housing_sn, town, resale_price from housing_list limit 40", null);
         return c;
     }
 }
