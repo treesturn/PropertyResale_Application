fc70102f87415c7870c42f4a006e432f7c16c755
Triston Chan <77474648+treesturn@users.noreply.github.com>
2022-10-17 22:33:37 +0800

    Improved item page layout

--- app/src/main/java/com/example/propertyresale_app/Item_page.java
+++ app/src/main/java/com/example/propertyresale_app/Item_page.java
@@ -16,29 +16,29 @@ public class Item_page extends AppCompatActivity {
 
         databaseHelper = new DatabaseHelper(this, "alpha_SQLite.db", 1);
 
-        TextView SN_item = findViewById(R.id.SN_item);
+//        TextView SN_item = findViewById(R.id.SN_item);
         TextView town_item = findViewById(R.id.town_item);
         TextView price_item = findViewById(R.id.price_item);
         TextView flattype_item = findViewById(R.id.flattype_item);
         TextView storey_item = findViewById(R.id.storey_item);
         TextView area_item = findViewById(R.id.area_item);
         TextView model_item = findViewById(R.id.model_item);
-        TextView agent_info = findViewById(R.id.agent_info);
+//        TextView agent_info = findViewById(R.id.agent_info);
         TextView interestedbuyers_item = findViewById(R.id.interestedbuyers_item);
 
         String sn = getIntent().getStringExtra("sn");
         String town = getIntent().getStringExtra("town");
-        String price = getIntent().getStringExtra("price");
+        String price = "$"+(getIntent().getStringExtra("price")) ;
 
 
-        SN_item.setText(sn);
+//        SN_item.setText(sn);
         town_item.setText(town);
         price_item.setText(price);
         flattype_item.setText(databaseHelper.gethousingtype(sn));
         storey_item.setText(databaseHelper.getstoreytype(sn));
         area_item.setText(databaseHelper.getareatype(sn));
         model_item.setText(databaseHelper.getmodeltype(sn));
-        agent_info.setText(databaseHelper.getagentinfo(sn));
+//        agent_info.setText(databaseHelper.getagentinfo(sn));
         interestedbuyers_item.setText(databaseHelper.getnumberofinterestedbuyers(sn));
 
 
